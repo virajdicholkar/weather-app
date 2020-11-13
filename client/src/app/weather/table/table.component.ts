115c40fdf351d822433ee9399771b80fdd4dfb96
Viraj Dicholkar <dicholkar.vv@gmail>
2020-11-13 09:37:10 +0530

    loaded weather data in table

--- client/src/app/weather/table/table.component.ts
+++ client/src/app/weather/table/table.component.ts
@@ -7,17 +7,28 @@ import { WeatherService } from '../weather.service';
   styleUrls: ['./table.component.scss']
 })
 export class TableComponent implements OnInit {
-
+  selectedWeather = {}
+  weatherData = []
   constructor(
     private weatherService: WeatherService
   ) { }
 
   ngOnInit(): void {
     this.weatherService.weatherData.subscribe(
-      data=>{
-        console.log('data', data)
+      data => {
+        this.initWeatherData(data);
       }
     )
+  }
+
+  initWeatherData(weatherData: Array<any>) {
+
+    this.weatherData = weatherData;
+    console.log('this.weatherData', this.weatherData)
+  }
+
+  convertKelvinToDegree(tempInKelvin) {
+    return tempInKelvin - 273.15;
   }
 
 }
